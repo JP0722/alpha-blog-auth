c173497a577f77ec6a45139ecc32b8868a2e6a84
JayaPrakash <pallaprakash07@gmail.com>
2022-11-10 20:12:39 +0530

    reInitialized git & created users table & model

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,55 @@
+class ArticlesController < ApplicationController
+
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
+	def index
+		@articles = Article.all
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def create
+		@article = Article.new(params.require(:article).permit(:title, :description))
+		if @article.save
+			flash[:notice] = "Article created successully"
+			redirect_to @article
+		else
+			render 'new'
+		end
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(params.require(:article).permit(:title, :description))
+			flash[:notice] = "Article updated successully"
+			redirect_to article_path(@article)
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		puts 'came to destroy'
+		@article = Article.find(params[:id])
+		@article.destroy
+		redirect_to articles_path
+	end
+
+
+	private
+
+	def print_debug(args)
+		puts "-"*20
+		puts args
+		puts "-"*20
+	end
+end
